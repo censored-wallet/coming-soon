14bcc6b2f65c9ecdd9fe2f7d4f522583bdf3cbb4
gewgawrav <gewgawrav@gmail.com>
2022-08-20 14:58:33 +0530

    Part 1

--- components/Header.js
+++ components/Header.js
@@ -2,18 +2,44 @@ export default function Header({ title }) {
   return (
     <>
       <h1 className="title">{title}</h1>
+      <h2>
+        Open Source database of web3 wallets that have been sanctioned or
+        identified by LEA during investigations around the world and are tied to
+        illegal activities such as money laundering, hacking, drug trafficking,
+        CSAM, scams, fraud and other illicit activities.
+      </h2>
+      <h4>Problem Statement</h4>
       <p>
-        Hi 👋! This template gives you a{" "}
-        <a href="https://nextjs.org/">Next.js</a> app with the scaffolding for{" "}
-        <a href="https://www.netlify.com/products/functions/">
-          Netlify Functions
-        </a>
-        , <a href="https://www.netlify.com/products/forms/">Forms</a>, and{" "}
-        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
-        with a few fun features.
+        More than 400 million wallets involving bitcoin, ethereum, USDT, and
+        other active cryptocurrencies have been detected on the blockchain. Due
+        to the anonymity of wallets, it becomes challenging to investigate,
+        collect evidence, collaborate for compliance, combat crime, and ensure a
+        secure web3 world.
+      </p>
+      <h4>Open Source Red Wallet </h4>
+      <h4>Methodology</h4>
+      <p>
+        Engage with Law enforcement agency and seek the list of wallet
+        identified for active monitoring and industry collabration
+      </p>
+      <h4>Traction</h4>
+      <p>
+        <ul>
+          <li>U.S. DEPARTMENT OF THE TREASURY OFAC sanctioned list</li>
+          <li>Israeli Government identified donation address</li>
+          <li>Partnerned with Indian LEAs</li>
+          <li>Milestone of 1,30,560 Red Wallets</li>
+        </ul>
       </p>
 
+      <h4>Roadmap</h4>
+      <p>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </p>
       <p>
         You can find the code for this project on GitHub at{" "}
         <a href="https://github.com/netlify-templates/nextjs-toolbox">
--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,23 @@ import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import FeedbackForm from "@components/FeedbackForm";
-import JokeBlock from "@components/JokeBlock";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Red Wallet - Open Source Red Wallets</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox" />
+        <Header title="What is Red Wallet?" />
         <hr />
         <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
+          Interested in integrating Red Wallets with your exchange, DApp, or
+          application then pleas?
         </p>
         <FeedbackForm />
-        <JokeBlock />
       </main>
       <Footer />
     </div>
